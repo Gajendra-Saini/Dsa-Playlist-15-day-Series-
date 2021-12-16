594675266d5e5b53f5a405fc5a0590d175fdb683
Gajendra Saini <rickysaini2003@gmail.com>
2021-12-16 11:10:01 +0530

    Day 4 completed

---
+++ day 4/introductionToArray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {5, 6, 4, 2, 7};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
